f23fe3bac82815d232ddc07f251e9d27d677d4a0
imrobinized <almigod0718@gmail.com>
2016-01-17 23:40:16 +0800

    add db config, routes, update package.json ...

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 
+var path = require('path');
+app.use(express.static(path.join(__dirname + '/public')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
+
+// setting up mongodb connection
 var mongoose = require('mongoose');
-
-
-app.use('/', function (req, res) {
-	res.send('anx-demo!');
+var dbconfig = require('./config/db_conn');
+mongoose.connect(dbconfig.url, function(err){
+	if(err) console.log(err);
+	else console.log('MongoDB connected! \nlink: ' + dbconfig.url);
 });
+
+// routes
+require('./routes/index.js')(app);
 
 var server = require('http').createServer(app);
 server.listen(port, function () {
---
+++ config/db_conn.js
@@ -0,0 +1,6 @@
+var mongolab = 'mongodb://adminadmin@ds037395.mongolab.com:37395/anx-demo';
+var local = 'mongodb://localhost:27017/bfbs';
+
+module.exports = {
+	'url'	: mongolab
+}
