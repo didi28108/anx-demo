c5a6f14412336c86d32fc603097a18a06b1943cf
imrobinized <almigod0718@gmail.com>
2016-01-17 23:40:49 +0800

    bring in Angular

---
+++ public/app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config( function($routeProvider) {
+		$routeProvider
+		.when('/', {
+				templateUrl: 'views/home.html'
+		})
+		.otherwise({
+				redirectTo: '/'
+		});
+});
