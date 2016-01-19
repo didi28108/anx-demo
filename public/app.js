cfa11d1044f2eaf1f2e1d589b2345c22826b0f09
imrobinized <almigod0718@gmail.com>
2016-01-19 14:33:14 +0800

    updating basic routing with angular and views

--- public/app.js
+++ public/app.js
@@ -1,11 +1,41 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider) {
-		$routeProvider
+myApp.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+
+	$routeProvider
 		.when('/', {
-				templateUrl: 'views/home.html'
+			templateUrl: 'views/home.html'
+		})
+		.when('/course', {
+			templateUrl: 'views/course.html'
+		})
+		.when('/news', {
+			templateUrl: 'views/news.html'
+		})
+		.when('/backend', {
+			templateUrl: 'views/backend-home.html'
+		})
+		.when('/backend/login', {
+			templateUrl: 'views/backend-login.html'
+		})
+		.when('/backend/test',  {
+			templateUrl: 'views/test.html'
 		})
 		.otherwise({
-				redirectTo: '/'
+			redirectTo: '/'
 		});
+
+});
+
+myApp.controller('loginCtrl', function($scope){
+		$scope.login = function (argument) {
+
+			var username = $scope.username;
+			var password = $scope.password;
+
+			if($scope.username == 'admin' && $scope.password == 'admin') {
+				$location.path('/backend/test');
+			}
+		}
 });
